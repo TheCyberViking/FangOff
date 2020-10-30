bc01110df53df3197a749a0100942c65ea4b73c1
Aardvark <gkimthomson@gmail.com>
2020-10-29 20:54:15 -0600

    Update FangOff.py
    Added a button to open URL, adding mailto: if appropriate. Added a default value to Entry line and if you hit Enter, the webbrowser opens the page.
    If the url is defanged, refang it before opening. If it contains any of the defanged values, don't defang again.

--- FangOff.py
+++ FangOff.py
@@ -1,21 +1,60 @@
 import tkinter as tk
+import webbrowser
 
 def defang(url):
-    str1 = url.replace(".", "[.]").replace("http", "hxxp").replace("https", "hxxps").replace("@", "[AT]")
-    master.clipboard_clear()
-    master.clipboard_append(str1)
+    '''Check and see if any of the defanged values appear and if
+    they do, just return, doing nothing. If they do not, fang it.'''
+    
+    defang_vals = ('hxxp', '[.]', '[AT]')
+    if any(val in url for val in defang_vals):
+        return
+    else:
+        str1 = url.replace(".", "[.]").replace("http", "hxxp").replace("https", "hxxps").replace("@", "[AT]")
+        update_url(str1)
 
 def fang(url):
+    '''remove the particular things we are looking for, make url dangerous again'''
     str1 = url.replace("[.]", ".").replace("hxxps", "https").replace("hxxp", "http").replace("[AT]", "@")
-    master.clipboard_clear()
-    master.clipboard_append(str1)
+    update_url(str1)
 
+def update_url(text):
+        '''just take the new value and update the url field
+        This will be run during both defang and fang'''
+        
+        url.delete(0, len(url.get()))
+        url.insert(0, text)
+
+def open_page():
+    '''check if any of the defanged values appear in the url field
+    if they do, refang it and open the page with webbrowser.
+    If they don't, just open it with webbrowser. If the url has an @
+    in it, add mailto: and open it in webbrowser.'''
+
+    defang_vals = ('hxxp', '[.]', '[AT]')
+    if any(val in url.get() for val in defang_vals):
+        fang(url.get())
+    
+    if '@' in url.get():
+        webbrowser.open(f'mailto:{url.get()}')
+    else:
+        webbrowser.open(url.get())
+
+#set up the master window
 master = tk.Tk()
 master.title("FangOff by @TheCyberViking")
-master.geometry("400x55")
+master.geometry("630x70")
+master.resizable(0, 0)
+
+#set up the Entry line with default value
 tk.Label(master, text="Enter URL: ").grid(row=0)
 url = tk.Entry(master, width=54)
 url.grid(row=0, column=1)
-tk.Button(master,text='De-Fang ',command=lambda: defang(url.get()) ).grid(row=3,column=0,sticky=tk.W,pady=4)
-tk.Button(master,text='Re-Fang ', command=lambda: fang(url.get())).grid(row=3,column=1,sticky=tk.W,pady=4)
+url.bind('<Return>', (lambda event: open_page())) #hit enter in the Entry and open the page
+url.insert(0, 'hxxps://github[.]com/TheCyberViking') #default value
+
+#set up buttons... two at the bottom, one after the Entry line
+tk.Button(master,text='De-Fang ',command=lambda: defang(url.get())).grid(row=3,column=0,sticky=tk.W,pady=4)
+tk.Button(master,text='Re-Fang ', command=lambda: fang (url.get())).grid(row=3,column=1,sticky=tk.W,pady=4)
+tk.Button(master,text='Open URL', command=lambda: open_page()).grid(row=0,column=2, sticky=tk.W,padx=4)
+
 tk.mainloop()
